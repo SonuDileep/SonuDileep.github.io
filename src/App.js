a9ee5810ac75524de4be700e1eb4be98c6dffbf7
SonuDileep <sonudileep@gmail.com>
2023-06-22 11:32:21 -0600

    Formating Fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import React, { Suspense, lazy } from 'react';
 import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Preloader from '../src/components/Pre'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import {Home} from './pages/Home.js'
 import {Project} from './pages/Project';
@@ -16,19 +16,16 @@ import {Whtelse} from './pages/Whatelse'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/projects" element={<Project/>} />
-            <Route path="/cv" element={<CV/>} />
-	    <Route path="/readings" element={<Reading />} />
-	    <Route path="/writings" element={<Writing />} />
-	    <Route path="/whatelse" element={<Whtelse/>} />
-
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/readings" element={<Reading />} />
+          <Route path="/writings" element={<Writing />} />
+          <Route path="/whatelse" element={<Whtelse />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
