4cd63df25e431b91d9dd0245c5f70ac2c6bb595d
SonuDileep <sonudileep@gmail.com>
2023-06-22 15:35:13 -0600

    test for browserRouter_1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Preloader from '../src/components/Pre'
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import {Home} from './pages/Home.js'
 import {Project} from './pages/Project';
@@ -16,16 +15,21 @@ import {Whtelse} from './pages/Whatelse'
 function App() {
   return (
     <div className="App">
-      <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Project />} />
-          <Route path='/cv' element={<CV />} />
-          <Route path='/readings' element={<Reading />} />
-          <Route path='/writings' element={<Writing />} />
-          <Route path='/whatelse' element={<Whtelse />} />
-      </Routes>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/projects" element={<Project/>} />
+            <Route path="/cv" element={<CV/>} />
+	    <Route path="/readings" element={<Reading />} />
+	    <Route path="/writings" element={<Writing />} />
+	    <Route path="/whatelse" element={<Whtelse/>} />
+
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
-export default App;
 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <App />
+  </React.StrictMode>
 );
