db2a777f62ebb75cd62fe725a5f3ac65b24986c5
SonuDileep <sonudileep@gmail.com>
2023-07-16 14:59:19 -0600

    Scheduel update

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -27,42 +27,42 @@ function SchedulePage() {
           <div style={{ marginTop: '10%' }}>
 	<div className="owner" style={{ textAlign: "left", fontSize: "20px" }}>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Daily_Updates.pptx?d=w5523b07c52e24a0e9c10224c411fe0ec&csf=1&web=1&e=WPHxtE" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Daily_Updates.pptx?d=w5523b07c52e24a0e9c10224c411fe0ec&csf=1&web=1&e=WPHxtE" className="btn btn-outline-default btn-round">
 	      Daily Updates
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Satellite%20Detection.pptx?d=w3f1e7ab7dfac4b6c9d5a8195f31c59b1&csf=1&web=1&e=isn7js" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Satellite%20Detection.pptx?d=w3f1e7ab7dfac4b6c9d5a8195f31c59b1&csf=1&web=1&e=isn7js" className="btn btn-outline-default btn-round">
 	      Satellite Detection
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/C3.pptx?d=w9b5447b1d4cf4943a50e57d4a5204fb1&csf=1&web=1&e=NXYfZ2" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/C3.pptx?d=w9b5447b1d4cf4943a50e57d4a5204fb1&csf=1&web=1&e=NXYfZ2" className="btn btn-outline-default btn-round">
 	      C3
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Smoke_Estimation.pptx?d=w6931015015f54aa8a7441e221607a87d&csf=1&web=1&e=rcSlYE" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Smoke_Estimation.pptx?d=w6931015015f54aa8a7441e221607a87d&csf=1&web=1&e=rcSlYE" className="btn btn-outline-default btn-round">
 	      Smoke Detection
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Long%20Video%20Understanding.pptx?d=w67b069734e8a490d9e56d384ba523690&csf=1&web=1&e=XF0Pho" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Long%20Video%20Understanding.pptx?d=w67b069734e8a490d9e56d384ba523690&csf=1&web=1&e=XF0Pho" className="btn btn-outline-default btn-round">
 	      Long Video Understanding
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/LLM%20COT.pptx?d=we250733f581f4c988369a16169ec4f94&csf=1&web=1&e=FOuK1M" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/LLM%20COT.pptx?d=we250733f581f4c988369a16169ec4f94&csf=1&web=1&e=FOuK1M" className="btn btn-outline-default btn-round">
 	      LLM
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Papers_TO_Read.pptx?d=w58ca69c88b7246c7820838ca06dbabbb&csf=1&web=1&e=rRwZRT" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Papers_TO_Read.pptx?d=w58ca69c88b7246c7820838ca06dbabbb&csf=1&web=1&e=rRwZRT" className="btn btn-outline-default btn-round">
 	      Papers To Read
 	    </a>
 	  </p>
 	  <p>
-	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Long%20Term%20Goals.pptx?d=wc2b7d178fb2f4b62809be33e06b85929&csf=1&web=1&e=reXKBA" className="btn-link mr-1 btn btn-success">
+	    <a href="https://colostate-my.sharepoint.com/:p:/r/personal/sonudilp_colostate_edu/Documents/Website_PPTs/Long%20Term%20Goals.pptx?d=wc2b7d178fb2f4b62809be33e06b85929&csf=1&web=1&e=reXKBA" className="btn btn-outline-default btn-round">
 	      Long Term Goals
 	    </a>
 	  </p>
